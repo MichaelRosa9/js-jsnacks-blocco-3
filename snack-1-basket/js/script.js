422c2340fa23f7baf7f79ccf0dfb3c2baeb9591f
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-27 16:35:55 +0200

    basket_player

--- snack-1-basket/js/script.js
+++ snack-1-basket/js/script.js
@@ -1,6 +1,6 @@
 //snack basket
 //definisco l'array di gicatori
-var team;
+var team= [];
 
 //definisco l'arrai di un sigolo giocatore
 var basket_player = {
@@ -10,14 +10,28 @@ var basket_player = {
 }
 
 $(function(){
-
-  var letters = randomLetter(3);
-  var name = letters + randomNumberGenerator(100, 999);
-
-
+  
+  while(team.length < 5){
+    var letters = randomLetter(3);
+    basket_player.playerName = letters + randomNumberGenerator(100, 999);
+    basket_player.ponints = randomNumberGenerator(0,50);
+    basket_player.threpointer_stats = percentage(randomNumberGenerator(0,100), randomNumberGenerator(0,100));
+    team.push(basket_player);
+    
+    console.log(basket_player);
+  }
+  
+  console.log(team);
 });
 
+//funzione che mi calcola la percentuale
+function percentage(num, perc){
+  var percentage = (num * perc) / 100;
+  return (num - percentage).toFixed(2);
+}
 
+
+//funzione che genera lettere maiuscole random
 function randomLetter(length){
   var result= [];
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
