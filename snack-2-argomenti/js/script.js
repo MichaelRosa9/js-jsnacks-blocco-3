967fc0ce09e470cd9c6c80fd027857731bca0d04
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-27 17:16:56 +0200

    snack 2 array

--- snack-2-argomenti/js/script.js
+++ snack-2-argomenti/js/script.js
@@ -4,14 +4,16 @@ arrayNumbers = [];
 for(i = 1; i<=100; i++){
   arrayNumbers.push(i);
 }
-console.log(arrayNumbers);
+console.log(funct(arrayNumbers, 10, 80));
 
 
 
 function funct(arr, num1, num2){
   var arrayReturn = [];
   if(num1 < num2 && num2<= arr.length){
-    
+    for(i = num1; i < arr.length; i++){
+      arrayReturn.push(i);
+    }
   }
   
   return arrayReturn
